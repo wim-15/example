8034292e937a28177ea2e002e8ff5db27ffc2d71
Wim <wim1595@gmail.com>
2022-11-05 15:13:10 +0100

    test

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm 2')
+    print_hi('PyCharm 2')
+    print_hi('PyCharm 2')
+    print_hi('PyCharm 2')
+    print_hi('PyCharm 2')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
